76ee805a53d7ad2763a1b6e8fdf133b9879cc40e
ZiembaPiotr <p.ziemba99@gmail.com>
2022-01-10 23:02:31 +0100

    Simple changes to menu

--- react-web/src/components/Authentication/registerBox.jsx
+++ react-web/src/components/Authentication/registerBox.jsx
@@ -6,6 +6,8 @@ const RegisterBox = () => {
   const [login, setLogin] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
 
   function onLoginChange(e) {
     setLogin(e.target.value);
@@ -17,6 +19,14 @@ const RegisterBox = () => {
 
   function onPasswordChange(e) {
     setPassword(e.target.value);
+  }
+
+  function onFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function onLastNameChange(e) {
+    setLastName(e.target.value);
   }
 
   return (
@@ -38,6 +48,24 @@ const RegisterBox = () => {
         placeholder="email"
         value={email}
         onChange={onEmailChange}
+      />
+      <input
+        type="text"
+        id="firstName"
+        className="fadeIn third"
+        name="firstName"
+        placeholder="first name"
+        value={firstName}
+        onChange={onFirstNameChange}
+      />
+      <input
+        type="text"
+        id="lastName"
+        className="fadeIn third"
+        name="lastName"
+        placeholder="last name"
+        value={lastName}
+        onChange={onLastNameChange}
       />
       <input
         type="password"
--- react-web/src/pages/menu.jsx
+++ react-web/src/pages/menu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/menu.css";
 import { Link, useHistory } from "react-router-dom";
 import { motion } from "framer-motion";
@@ -13,9 +13,12 @@ import Login from "./login";
 const Menu = () => {
   let history = useHistory();
 
+  const [player1Data, setPlayer1Data] = useState([{}, false]);
+  const [player2Data, setPlayer2Data] = useState([{}, false]);
+
   return (
     <div className="mainContainer">
-      <Login />
+      <Login setPlayerData={setPlayer1Data} playerData={player1Data}/>
       <div className="column">
         <motion.h5
           className="battleshipsText"
@@ -53,6 +56,10 @@ const Menu = () => {
         <motion.div
           className="chooseMode"
           onClick={() => {
+            if (!player2Data[0].login || !player1Data[0].login) {
+              alert("You have to log in on both accounts to play");
+              return;
+            }
             history.push("/hot-seat");
           }}
           initial={leftBoxVariants.hidden}
@@ -88,7 +95,7 @@ const Menu = () => {
           <h5 className="chooseModeText">Saved sea battles</h5>
         </motion.div>
       </div>
-      <Login />
+      <Login setPlayerData={setPlayer2Data} playerData={player2Data} />
     </div>
   );
 };
